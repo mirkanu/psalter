548e50e7565f17f4d3bce0ddad4efcd73ad30317
Manuel Kuhs <manuelkuhs@gmail.com>
2026-06-15 10:18:12 +0000

    fix(05): restore Link import in SetItemRow accidentally removed in task 3
    - Link is still needed for the play-precenting action button

--- src/components/precent/SetItemRow.tsx
+++ src/components/precent/SetItemRow.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import Link from 'next/link'
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 import { GripVertical, PlayCircle, Trash2 } from 'lucide-react'
